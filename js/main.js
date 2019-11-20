2c3ad0d612631d401bfe0259d951fe013194e8b7
ismael faro <ismael.faro1@ibm.com>
2019-11-20 03:05:13 -0500

    Add devicePixelRatio and split the code.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    var canvas = document.getElementById('renderCanvas');
+
+    var config = {
+        devicePixelRatio: window.devicePixelRatio || 1.0,
+        fontSize: 28 * devicePixelRatio
+    };
+
+    // load the 3D engine
+    var engine = new BABYLON.Engine(canvas, true);
+
+    engine.setHardwareScalingLevel(1.0 / config.devicePixelRatio);
+    console.log('----------> devicePixelRatio:', devicePixelRatio)
+
+    // call the createScene function
+    var scene = createScene(engine, canvas, config);
+
+    // run the render loop
+    engine.runRenderLoop(function() {
+        scene.render();
+    });
+
+    // the canvas/window resize event handler
+    window.addEventListener('resize', function() {
+        engine.resize();
+    });
+});
